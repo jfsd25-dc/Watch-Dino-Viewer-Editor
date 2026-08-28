fc64042668dae692a35e8c1f1de9e98067032561
jfsd25-dc <johnfrancis.dagman25@gmail.com>
2026-08-28 16:09:04 +0800

    Simplify AELE widget packaging after successful compile

--- build_aele_widget_dmg.sh
+++ build_aele_widget_dmg.sh
@@ -24,66 +24,7 @@ xcodebuild \
 test -d "$APP"
 test -d "$WIDGET"
 
-# Create an icon locally so the project has no binary source dependencies.
-cat > "$BUILD/make_icon.swift" <<'SWIFT'
-import AppKit
-
-let output = CommandLine.arguments[1]
-let size = NSSize(width: 1024, height: 1024)
-let image = NSImage(size: size)
-image.lockFocus()
-
-NSColor(calibratedRed: 0.025, green: 0.025, blue: 0.03, alpha: 1).setFill()
-NSBezierPath(roundedRect: NSRect(x: 48, y: 48, width: 928, height: 928), xRadius: 220, yRadius: 220).fill()
-
-NSColor(calibratedWhite: 1, alpha: 0.08).setFill()
-NSBezierPath(roundedRect: NSRect(x: 148, y: 170, width: 728, height: 684), xRadius: 92, yRadius: 92).fill()
-
-let centered = NSMutableParagraphStyle()
-centered.alignment = .center
-
-let aele: [NSAttributedString.Key: Any] = [
-    .font: NSFont.systemFont(ofSize: 130, weight: .bold),
-    .foregroundColor: NSColor(calibratedWhite: 0.92, alpha: 1),
-    .paragraphStyle: centered,
-    .kern: 12
-]
-("AELE" as NSString).draw(in: NSRect(x: 150, y: 590, width: 724, height: 170), withAttributes: aele)
-
-let digits: [NSAttributedString.Key: Any] = [
-    .font: NSFont.monospacedDigitSystemFont(ofSize: 300, weight: .black),
-    .foregroundColor: NSColor(calibratedWhite: 0.98, alpha: 1),
-    .paragraphStyle: centered
-]
-("26" as NSString).draw(in: NSRect(x: 150, y: 260, width: 724, height: 350), withAttributes: digits)
-
-image.unlockFocus()
-guard let tiff = image.tiffRepresentation,
-      let rep = NSBitmapImageRep(data: tiff),
-      let png = rep.representation(using: .png, properties: [:]) else {
-    fatalError("Unable to create icon")
-}
-try png.write(to: URL(fileURLWithPath: output))
-SWIFT
-
-xcrun swift "$BUILD/make_icon.swift" "$BUILD/icon-1024.png"
-ICONSET="$BUILD/AppIcon.iconset"
-mkdir -p "$ICONSET"
-for SPEC in "16 16" "16 32" "32 32" "32 64" "128 128" "128 256" "256 256" "256 512" "512 512" "512 1024"; do
-  set -- $SPEC
-  LOGICAL="$1"
-  PIXELS="$2"
-  if [ "$LOGICAL" = "$PIXELS" ]; then
-    NAME="icon_${LOGICAL}x${LOGICAL}.png"
-  else
-    NAME="icon_${LOGICAL}x${LOGICAL}@2x.png"
-  fi
-  sips -z "$PIXELS" "$PIXELS" "$BUILD/icon-1024.png" --out "$ICONSET/$NAME" >/dev/null
-done
-iconutil -c icns "$ICONSET" -o "$APP/Contents/Resources/AppIcon.icns"
-/usr/libexec/PlistBuddy -c "Set :CFBundleIconFile AppIcon" "$APP/Contents/Info.plist"
-
-# Ad-hoc sign the extension first, then the containing app.
+# Ad-hoc sign the WidgetKit extension first, then the containing app.
 codesign --force --sign - \
   --entitlements "$ROOT/AELECountdownWidget/AELECountdownWidget.entitlements" \
   "$WIDGET"
